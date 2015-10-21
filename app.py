d8596ebba50eb0b9035f6f075e21beb88f940517
Chris <crb828@ymail.com>
2015-10-20 19:19:16 -0500

    changed the laurates for linking in the future

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ prize_rows[3] = dict(url='/1964/chemistry', numLaureate=1, year=1964, category='
 
 laurate_rows = dict()
 #dict key = id
-laurate_rows[1] = dict(url='wilhelm/röntgen', id=1, name='Wilhelm Röntgen', year=1901, prizes=1, dob='1845-03-27', gender='M')
-laurate_rows[2] = dict(url='hendrik/lorentz', id=2, name='Hendrik Lorentz', year=1902, prizes=1, dob='1853-07-18', gender='M')
-laurate_rows[3] = dict(url='pieter/zeeman', id=3, name='Pieter Zeeman', year=1902, prizes=1, dob='1865-05-25', gender='M')
+laurate_rows[1] = dict(url='laurates/dorothy_crowfoot_hodgkin', id=230, name='Dorothy Crowfoot Hodgkin', year=1964, prizes=1, dob='1910-05-12', gender='F')
+laurate_rows[2] = dict(url='laurates/paul_krugman', id=834, name='Paul Krugman', year=2008, prizes=1, dob='1953-02-28', gender='M')
+laurate_rows[3] = dict(url='laurates/takaaki_kajita', id=919, name='Takaaki Kajita', year=2015, prizes=1, dob='0000-00-00', gender='M')
 
 country_rows = dict()
 #dict key = id
-country_rows[1] = dict(url='countries/Afghanistan', id=1, name='Afghanistan', numPrizes=0, numLaureates=0, pop=26023100)
+country_rows[1] = dict(url='countries/afghanistan', id=1, name='Afghanistan', numPrizes=0, numLaureates=0, pop=26023100)
 country_rows[2] = dict(url='countries/unitedstates', id=2, name='United States', numPrizes=356, numLaureates=356, pop=321645000)
 country_rows[3] = dict(url='countries/unitedkingdom', id=3, name='United Kingdom', numPrizes=116, numLaureates=116, pop=64800000)
 
@@ -41,10 +41,11 @@ def render_1964_chemistry():
 
 @app.route("countries")
 def render_countries():
-	return render_template('country_template.html' entry = country_rows[3])
+	return render_template('country_template.html', entry = country_rows[3])
 
 @app.route("laurate")
 def render_laurates():
-	return render_template('laureate_template')
+	return render_template('laureate_template', entry = laurate_rows[3])
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
