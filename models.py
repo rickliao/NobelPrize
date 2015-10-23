ea8c639c6c56f6d552539efe00e58e34fb53655c
Ellen Wiberg <ellen.wiberg@utexas.edu>
2015-10-22 19:36:53 -0500

    Added comments to models

--- models.py
+++ models.py
@@ -1,20 +1,32 @@
 from app import db
 
+"""
+Additional table to map the many to many relationship between Prize and Laureate
+Maps laureate id to prize id, with the values being a foreign key representing
+the id attribute in each table
+"""
 laureates = db.Table('tags',
     db.Column('laureate_id', db.Integer, db.ForeignKey('laureate.id')),
     db.Column('prize_id', db.Integer, db.ForeignKey('prize.id'))
 )
 
-
+"""
+Class representing the Prize table
+Class variables are columns
+Each instance of this class is a row
+"""
 class Prize(db.Model) :
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(80))
     year = db.Column(db.Integer)
     nr_laureates = db.Column(db.Integer)
     motivation = db.Column(db.String(250))
+    
+    #Many to many relationship
     laureates = db.relationship('Laureate', secondary=laureates,
             backref=db.backref('prizes', lazy='dynamic'))
 
+    # Constructor
     def __init__(self, category, year, nr_laureates, motivation):
         self.category = category
         self.year = year
@@ -24,14 +36,23 @@ class Prize(db.Model) :
     def __repr__(self):
         return '<Prize %r>' % self.category
 
+
+"""
+Class representing the Laureate table
+Class variables are columns
+Each instance of this class is a row
+"""
 class Laureate(db.Model) :
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     nr_prizes = db.Column(db.Integer)
     date_of_birth = db.Column(db.DateTime)
     gender = db.Column(db.String(1))
+    
+    #One to many relationship
     country_id = db.Column(db.Integer, db.ForeignKey('country.country_code'))
 
+    #Constructor
     def __init__(self, name, nr_prizes, date_of_birth, gender) :
         self.name = name
         self.nr_prizes = nr_prizes
@@ -41,14 +62,22 @@ class Laureate(db.Model) :
     def __repr__(self) :
         return '<Laureate %r>' % self.name
 
+"""
+Class representing the Country table
+Class variables are columns
+Each instance of this class is a row
+"""
 class Country(db.Model) :
     country_code = db.Column(db.String(2), primary_key=True)
     name = name = db.Column(db.String(80))
     nr_laureates = db.Column(db.Integer)
     nr_prizes = db.Column(db.Integer)
     population = db.Column(db.Integer)
+    
+    #One to many relationship
     laureates = db.relationship('Laureate', backref='country', lazy='dynamic')
     
+    #Constructor
     def __init__(self, country_code, name, nr_laureates, nr_prizes, population) :
         self.country_code = country_code
         self.name = name
