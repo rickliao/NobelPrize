cb1874a1a0d0e42ea4c95e013b064d7ecafe0797
User <rickliao>
2015-10-29 17:16:31 +0000

    added images to all pages

--- tempApp.py
+++ tempApp.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from models import Prize, Laureate, Country
+import requests
+import json
+from urllib.parse import quote
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://nobeladmin:cs373Prize@localhost/nobeldb'
@@ -59,7 +62,12 @@ def render_individual_prize(myYear, myCategory):
         laureateNames += [laureate.name]
         laureateUrls += [laureate.url]
     entry = {'year':prize.year, 'category':prize.category, 'motivation':prize.motivation, 'numLaureate':len(laureateNames), 'laureate':laureateNames, 'laureateUrl':laureateUrls}
-    return render_template('prize_template.html', entry = entry)
+
+    # embed image
+    search = "nobel prize "+str(prize.year)+" "+prize.category
+    url, width, height = getImage(search)
+
+    return render_template('prize_template.html', entry = entry, imageUrl = url, width = width, height = height)
 
 @app.route("/laureates/<myName>")
 def render_individual_laureate(myName):
@@ -86,7 +94,11 @@ def render_individual_laureate(myName):
         countryUrl = "/error"
 
     entry = {'name':laureate.name, 'year':year, 'numPrizes':laureate.nr_prizes, 'prizes':prizesList, 'prizesUrl':prizesUrlList, 'dob':laureate.date_of_birth, 'gender':laureate.gender, 'country':countryName, 'countryUrl':countryUrl}
-    return render_template('laureate_template.html', entry = entry)
+
+    # embed image
+    url, width, height = getImage(laureate.name)
+
+    return render_template('laureate_template.html', entry = entry, imageUrl = url, width = width, height = height)
 
 @app.route("/countries/<myName>")
 def render_individual_countries(myName):
@@ -94,11 +106,30 @@ def render_individual_countries(myName):
 
     entry = {'name':country.name, 'code':country.country_code, 'numLaureates':country.nr_laureates, 'numPrizes':country.nr_prizes, 'pop':country.population}
 
-    return render_template('country_template.html', entry = entry)
+    # embed image
+    url, width, height = getImage(country.name)
+
+    return render_template('country_template.html', entry = entry, imageUrl = url, width = width, height = height)
 
 @app.route("/about")
 def render_about():
 	return render_template('about.html');
+
+def getImage(query):
+    # embed image
+    response = requests.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+quote(query))
+    result = json.loads(response.text)
+   
+    # scale image 
+    firstResult = result['responseData']['results'][0]
+    width = int(firstResult['width'])
+    height = int(firstResult['height'])
+    while width > 500 or height > 500:
+        width /= 2
+        height /= 2
+
+    print(width, height)
+    return (firstResult['url'], width, height)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
