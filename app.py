00e80c48332e23825eb748ba04f77c7e8b4c1a34
User <rickliao>
2015-11-03 22:49:00 +0000

    rename app

--- app.py
+++ app.py
@@ -1,99 +1,151 @@
-from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import render_template, abort
+from db import app
+from models import Prize, Laureate, Country
+import requests
+import json
+from urllib.parse import quote
 import os
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://nobeladmin:cs373Prize@localhost/nobeldb'
-db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-prize_rows = dict()
-# dict key = id
-prize_rows[1] = dict(url='/2008/economics', numLaureate=1, year=2008, category='Economics', motivation='\"for his analysis of trade patterns and location of economic activity\"', laureate=['Paul Krugman'], laureateUrl=['/laureates/paul_krugman'])
-prize_rows[2] = dict(url='/2015/physics', numLaureate=2, year=2015, category='Physics', motivation='\"for the discovery of neutrino oscillations, which shows that neutrinos have mass\"', laureate=['Takaaki Kajita'], laureateUrl=['/laureates/takaaki_kajita'])
-prize_rows[3] = dict(url='/1964/chemistry', numLaureate=1, year=1964, category='Chemistry', motivation='\"for her determinations by X-ray techniques of the structures of important biochemical substances\"', laureate=['Dorothy Crowfoot Hodgkin'], laureateUrl=['/laureates/dorothy_crowfoot_hodgkin'])
-
-laureate_rows = dict()
-#dict key = id
-laureate_rows[230] = dict(url='/laureates/dorothy_crowfoot_hodgkin', name='Dorothy Crowfoot Hodgkin', year=1964, numPrizes=1, dob='1910-05-12', gender='F', country='UK', countryUrl='/countries/unitedkingdom', prizes=['Chemistry: 1964'], prizesUrl=['/1964/chemistry'])
-laureate_rows[834] = dict(url='/laureates/paul_krugman', name='Paul Krugman', year=2008, numPrizes=1, dob='1953-02-28', gender='M', country='US', countryUrl='/countries/unitedstates', prizes=['Economics: 2008'], prizesUrl=['/2008/economics'])
-laureate_rows[919] = dict(url='/laureates/takaaki_kajita', name='Takaaki Kajita', year=2015, numPrizes=1, dob='0000-00-00', gender='M', country='JP', countryUrl='/countries/japan', prizes=['Physics: 2015'], prizesUrl=['/2015/physics'])
-
-country_rows = dict()
-#dict key = id
-# country_rows[1] = dict(url='/countries/afghanistan', code='AF', name='Afghanistan', numPrizes=0, numLaureates=0, pop=26023100)
-country_rows[1] = dict(url='/countries/unitedstates', code= 'US',  name='United States', numPrizes=356, numLaureates=356, pop=321645000)
-country_rows[2] = dict(url='/countries/unitedkingdom', code='UK', name='United Kingdom', numPrizes=116, numLaureates=116, pop=64800000)
-country_rows[3] = dict(url='/countries/japan', code='JP', name='Japan', numPrizes=24, numLaureates=24, pop=126865000)
-
 @app.route("/prize")
 def render_prize():
-    return render_template('prize.html', entries = prize_rows)
+    prizes = Prize.query.filter_by().all()
+    return render_template('prize_db.html', prizes = prizes)
 
-@app.route("/2008/economics")
-def render_2008_economics():
-	return render_template('economics_2008.html', entry = prize_rows[1])
+@app.route("/laureates")
+def render_laureate():
+    laureates = Laureate.query.filter_by().all()
+    entries = []
+    for laureate in laureates:
+        prizes = laureate.prizes.all()
+        prizesList = []
+        prizesUrlList = []
+        for prize in prizes:
+            prizeName = prize.category+": "+str(prize.year)
+            prizesList += [prizeName]
+            prizesUrlList += [prize.url]
 
-@app.route("/2015/physics")
-def render_2015_physics():
-	return render_template('physics_2015.html', entry = prize_rows[2])
+        if not (laureate.country_id == None):
+            country = Country.query.get(laureate.country_id)
+            countryName = country.name
+            countryUrl = country.url
+        else:
+           countryName = "data unavailable"
+           countryUrl = "/error"
+        
+        entry = {'url':laureate.url, 'id':laureate.id, 'name':laureate.name, 'numPrizes':laureate.nr_prizes, 'prizes':prizesList, 'prizesUrl':prizesUrlList, 'dob':laureate.date_of_birth, 'gender':laureate.gender, 'country':countryName, 'countryUrl':countryUrl}
+        entries += [entry]
 
-@app.route("/1964/chemistry")
-def render_1964_chemistry():
-	return render_template('chemistry_1964.html', entry = prize_rows[3])
+    return render_template('laureate_db.html', entries = entries)
 
 @app.route("/countries")
 def render_countries():
-	return render_template('country.html', entries = country_rows)
+    countries = Country.query.filter_by().all()
+    return render_template('country_db.html', countries = countries)
 
-@app.route("/countries/unitedkingdom")
-def render_unitedkingdom():
-	return render_template('unitedkingdom.html', entry = country_rows[2])
 
-@app.route("/countries/unitedstates")
-def render_unitedstates():
-	return render_template('unitedstates.html', entry = country_rows[1])
+@app.route("/prizes/<myYear>/<myCategory>")
+def render_individual_prize(myYear, myCategory):
+    prize = Prize.query.filter_by(year = myYear, category = myCategory).first()
+    if prize == None:
+        abort(404)
 
-@app.route("/countries/japan")
-def render_japan():
-	return render_template('japan.html', entry = country_rows[3])
+    laureates = prize.laureates
+    laureateNames = []
+    laureateUrls = []
+    for laureate in laureates:
+        laureateNames += [laureate.name]
+        laureateUrls += [laureate.url]
+    entry = {'year':prize.year, 'category':prize.category, 'motivation':prize.motivation, 'numLaureate':len(laureateNames), 'laureate':laureateNames, 'laureateUrl':laureateUrls}
 
-@app.route("/laureates")
-def render_laurates():
-	return render_template('laureate.html', entries = laureate_rows)
+    # embed image
+    search = "nobel prize "+str(prize.year)+" "+prize.category
+    url, width, height = getImage(search)
 
-@app.route("/laureates/paul_krugman")
-def render_paul_krugman():
-	return render_template('paul_krugman.html', entry = laureate_rows[834])
+    return render_template('prize_template.html', entry = entry, imageUrl = url, width = width, height = height)
 
-@app.route("/laureates/takaaki_kajita")
-def render_takaaki_kajita():
-	return render_template('takaaki_kajita.html', entry = laureate_rows[919])
+@app.route("/laureates/<myName>")
+def render_individual_laureate(myName):
+    laureate = Laureate.query.filter_by(name=myName.replace("_"," ")).first()
+    if laureate == None:
+        abort(404)
+    
+    prizes = laureate.prizes.all()
+    prizesList = []
+    prizesUrlList = []
+    year = 0 
+    for prize in prizes:
+        prizeName = prize.category+": "+str(prize.year)
+        prizesList += [prizeName]
+        prizesUrlList += [prize.url]
+        year = prize.year
 
-@app.route("/laureates/arthur_mcdonald")
-def render_arthur_mcdonald():
-	return "Haven't made the page, only three instances of laureates required";
+    if not (laureate.country_id == None):
+        country = Country.query.get(laureate.country_id)
+        countryName = country.name
+        countryUrl = country.url
+    else:
+        countryName = "data unavailable"
+        countryUrl = "/error"
 
-@app.route("/laureates/dorothy_crowfoot_hodgkin")
-def render_dorothy_crowfoot_hodgkin():
-	return render_template('dorothy_crowfoot_hodgkin.html', entry = laureate_rows[230])
+    entry = {'name':laureate.name, 'year':year, 'numPrizes':laureate.nr_prizes, 'prizes':prizesList, 'prizesUrl':prizesUrlList, 'dob':laureate.date_of_birth, 'gender':laureate.gender, 'country':countryName, 'countryUrl':countryUrl}
+
+    # embed image
+    url, width, height = getImage(laureate.name)
+
+    return render_template('laureate_template.html', entry = entry, imageUrl = url, width = width, height = height)
+
+@app.route("/countries/<myName>")
+def render_individual_countries(myName):
+    country = Country.query.filter_by(name = myName.replace("_", " ")).first()
+
+    entry = {'name':country.name, 'code':country.country_code, 'numLaureates':country.nr_laureates, 'numPrizes':country.nr_prizes, 'pop':country.population}
+
+    # embed image
+    url, width, height = getImage(country.name)
+
+    return render_template('country_template.html', entry = entry, imageUrl = url, width = width, height = height)
 
 @app.route("/about")
 def render_about():
-	return render_template('about.html');
+    return render_template('about.html');
 
-@app.route("/aboutT/")
+def getImage(query):
+    # embed image
+    response = requests.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+quote(query))
+    result = json.loads(response.text)
+   
+    # scale image 
+    firstResult = result['responseData']['results'][0]
+    width = int(firstResult['width'])
+    height = int(firstResult['height'])
+    while width > 500 or height > 500:
+        width /= 2
+        height /= 2
+
+    print(width, height)
+    return (firstResult['url'], width, height)
+
+@app.route("/aboutT")
 def test_link():
-	print ('my test fun works')
-	os.system("make test")
-	f = open("TestModels.tmp")
-	result = ""
-	for line in f:
-		result += line
-	return render_template('aboutT.html', result=result);
+    os.system("make test")
+    f = open("TestModels.tmp", 'r')
+    result = []
+    for line in f:
+        result += [line]
+    os.system("make clean")    
+    return render_template('aboutT.html', result=result);
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('error.html'), 404
+
+@app.route("/error")
+def error():
+    return render_template('error.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=3000)
