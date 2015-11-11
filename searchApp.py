99b8a6489105dc11d4f4ed009ad7bd328e1eac55
Rick <rickliao@utexas.edu>
2015-11-11 07:01:33 +0000

    search with and

---
+++ searchApp.py
@@ -0,0 +1,166 @@
+from flask import render_template, abort
+from db import db, app
+from flask.ext.restless import APIManager
+from models import Prize, Laureate, Country
+import requests
+import json
+from urllib.parse import quote
+import os
+from search_db import searchTermAnd
+
+manager = APIManager(app, flask_sqlalchemy_db=db)
+manager.create_api(Prize, methods=['GET'], include_columns=['id', 'category', 'laureates', 'year', 'nr_laureates'])
+manager.create_api(Laureate, methods=['GET'], include_columns=['id', 'name', 'country_id', 'date_of_birth', 'gender', 'nr_prizes'])
+manager.create_api(Country, methods=['GET'], include_columns=['country_code', 'name', 'laureates', 'nr_laureates', 'nr_prizes', 'population'])
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/prize")
+def render_prize():
+    prizes = Prize.query.filter_by().all()
+    return render_template('prize_db.html', prizes = prizes)
+
+@app.route("/laureates")
+def render_laureate():
+    laureates = Laureate.query.filter_by().all()
+    entries = []
+    for laureate in laureates:
+        prizes = laureate.prizes.all()
+        prizesList = []
+        prizesUrlList = []
+        for prize in prizes:
+            prizeName = prize.category+": "+str(prize.year)
+            prizesList += [prizeName]
+            prizesUrlList += [prize.url]
+
+        if not (laureate.country_id == None):
+            country = Country.query.get(laureate.country_id)
+            countryName = country.name
+            countryUrl = country.url
+        else:
+           countryName = "N/A"
+           countryUrl = "/error"
+        
+        entry = {'url':laureate.url, 'id':laureate.id, 'name':laureate.name, 'numPrizes':laureate.nr_prizes, 'prizes':prizesList, 'prizesUrl':prizesUrlList, 'dob':laureate.date_of_birth, 'gender':laureate.gender, 'country':countryName, 'countryUrl':countryUrl}
+        entries += [entry]
+
+    return render_template('laureate_db.html', entries = entries)
+
+@app.route("/countries")
+def render_countries():
+    countries = Country.query.filter_by().all()
+    return render_template('country_db.html', countries = countries)
+
+
+@app.route("/prizes/<myYear>/<myCategory>")
+def render_individual_prize(myYear, myCategory):
+    prize = Prize.query.filter_by(year = myYear, category = myCategory).first()
+    if prize == None:
+        abort(404)
+
+    laureates = prize.laureates
+    laureateNames = []
+    laureateUrls = []
+    for laureate in laureates:
+        laureateNames += [laureate.name]
+        laureateUrls += [laureate.url]
+    entry = {'year':prize.year, 'category':prize.category.capitalize(), 'motivation':prize.motivation, 'numLaureate':len(laureateNames), 'laureate':laureateNames, 'laureateUrl':laureateUrls}
+
+    # embed image
+    search = "nobel prize "+str(prize.year)+" "+prize.category
+    url, width, height = getImage(search)
+
+    return render_template('prize_template.html', entry = entry, imageUrl = url, width = width, height = height)
+
+@app.route("/laureates/<myName>")
+def render_individual_laureate(myName):
+    laureate = Laureate.query.filter_by(name=myName.replace("_"," ")).first()
+    if laureate == None:
+        abort(404)
+    
+    prizes = laureate.prizes.all()
+    prizesList = []
+    prizesUrlList = []
+    year = 0 
+    for prize in prizes:
+        prizeName = prize.category+": "+str(prize.year)
+        prizesList += [prizeName]
+        prizesUrlList += [prize.url]
+        year = prize.year
+
+    if not (laureate.country_id == None):
+        country = Country.query.get(laureate.country_id)
+        countryName = country.name
+        countryUrl = country.url
+    else:
+        countryName = "N/A"
+        countryUrl = "/error"
+
+    entry = {'name':laureate.name, 'year':year, 'numPrizes':laureate.nr_prizes, 'prizes':prizesList, 'prizesUrl':prizesUrlList, 'dob':laureate.date_of_birth, 'gender':laureate.gender, 'country':countryName, 'countryUrl':countryUrl}
+
+    # embed image
+    url, width, height = getImage(laureate.name)
+
+    return render_template('laureate_template.html', entry = entry, imageUrl = url, width = width, height = height)
+
+@app.route("/countries/<myName>")
+def render_individual_countries(myName):
+    country = Country.query.filter_by(name = myName.replace("_", " ")).first()
+    if country == None:
+        abort(404)
+
+    entry = {'name':country.name, 'code':country.country_code, 'numLaureates':country.nr_laureates, 'numPrizes':country.nr_prizes, 'pop':country.population}
+
+    # embed image
+    url, width, height = getImage(country.name)
+
+    return render_template('country_template.html', entry = entry, imageUrl = url, width = width, height = height)
+
+@app.route("/about")
+def render_about():
+    return render_template('about.html');
+
+def getImage(query):
+    # embed image
+    response = requests.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="+quote(query))
+    result = json.loads(response.text)
+   
+    # scale image 
+    firstResult = result['responseData']['results'][0]
+    width = int(firstResult['width'])
+    height = int(firstResult['height'])
+    while width > 500 or height > 500:
+        width /= 2
+        height /= 2
+
+    print(width, height)
+    return (firstResult['url'], width, height)
+
+@app.route("/aboutT")
+def test_link():
+    os.system("make test")
+    f = open("TestModels.tmp", 'r')
+    result = []
+    for line in f:
+        result += [line]
+    f.close()
+    os.system("make clean")    
+    return render_template('aboutT.html', result=result);
+
+@app.route("/search/<query>")
+def render_search(query):
+    links, titles, contexts = searchTermAnd(query)
+    return render_template("search_single.html", term = query, titles = titles, links = links, contexts = contexts)
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('error.html'), 404
+
+@app.route("/error")
+def error():
+    return render_template('error.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
--- search_db.py
+++ search_db.py
@@ -1,31 +1,36 @@
 from models import Prize, Laureate, Country
 import re
 
-def searchTerm(term):
+def searchTermAnd(term):
     links = []
     contexts = []
-    title = []
+    titles = []
+    terms = term.split(" ")
 
     prizes = Prize.query.filter_by().all()
     for prize in prizes:
-        searchString = " " + prize.category.capitalize()
+        searchString = prize.category.capitalize()
         searchString += " Year: " + str(prize.year)
         searchString += " Number of Laureate: " + str(prize.nr_laureates)
         searchString += " Laureate:"
         for laureate in prize.laureates[:-1]:
             searchString += " " + laureate.name+","
         searchString += " " + prize.laureates[-1].name
-        searchString += " Motivation: " + prize.motivation + " "
+        searchString += " Motivation: " + prize.motivation
         
-        match = re.search("([^ ]*) "+term+" ([^ ]*)", searchString, re.IGNORECASE)
-        if match != None:
+        inString = True
+        for query in terms:
+            if query.lower() not in searchString.lower():
+                inString = False
+        #match = re.search("([^ ]*)"+term+"([^ ]*)", searchString, re.IGNORECASE)
+        if inString:
             links += [prize.url]
-            title += [prize.category.capitalize() + ": " + str(prize.year)]
-            contexts += [match.group(0)]
+            titles += [prize.category.capitalize() + ": " + str(prize.year)]
+            contexts += [searchString]
 
     laureates = Laureate.query.filter_by().all()
     for laureate in laureates:
-        searchString = " " + laureate.name
+        searchString = laureate.name
         searchString += " Number of Prizes: " + str(laureate.nr_prizes)
         searchString += " Prizes:"
         laureatePrizes = laureate.prizes.all()
@@ -40,30 +45,37 @@ def searchTerm(term):
             searchString += " Country: N/A "
         else:        
             country = Country.query.get(laureate.country_id)
-            searchString += " Country: " + country.name + " "
- 
-        match = re.search("([^ ]*) "+term+" ([^ ]*)", searchString, re.IGNORECASE)
-        if match != None:
+            searchString += " Country: " + country.name
+        
+        inString = True
+        for query in terms:
+            if query.lower() not in searchString.lower():
+                inString = False
+        #match = re.search("([^ ]*)"+term+"([^ ]*)", searchString, re.IGNORECASE)
+        if inString:
             links += [laureate.url]
-            title += [laureate.name]
-            contexts += [match.group(0)]
+            titles += [laureate.name]
+            contexts += [searchString]
 
     countries = Country.query.filter_by().all()
     for country in countries:
-        searchString = " " + country.name
+        searchString = country.name
         searchString += " Country Code: " + country.country_code
         searchString += " Population: " + str(country.population)
         searchString += " Number of Laureate: " + str(country.nr_laureates)
-        searchString += " Number of Prizes: " + str(country.nr_prizes) + " "
+        searchString += " Number of Prizes: " + str(country.nr_prizes)
         
-        match = re.search("([^ ]*) "+term+" ([^ ]*)", searchString, re.IGNORECASE)
-        if match != None:
+        inString = True
+        for query in terms:
+            if query.lower() not in searchString.lower():
+                inString = False
+        #match = re.search("([^ ]*)"+term+"([^ ]*)", searchString, re.IGNORECASE)
+        if inString:
             links += [country.url]
-            title += [country.name]
-            contexts += [match.group(0)]
-    print(links)
-    print(title)
-    print(contexts)
+            titles += [country.name]
+            contexts += [searchString]
+    
+    return (links, titles, contexts)
 
 if __name__ == "__main__" :
-    searchTerm("2013")
+    print(searchTermAnd("taiwan"))
