7d6865c97b17dab32ea9d32bdaf32b04e57223c9
Rick <rickliao@utexas.edu>
2015-11-12 04:43:04 +0000

    search finished except highlight

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ from urllib.parse import quote
 import os
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(Prize, methods=['GET'], include_columns=['id', 'category', 'laureates', 'year', 'nr_laureates'])
-manager.create_api(Laureate, methods=['GET'], include_columns=['id', 'name', 'country_id', 'date_of_birth', 'gender', 'nr_prizes'])
-manager.create_api(Country, methods=['GET'], include_columns=['country_code', 'name', 'laureates', 'nr_laureates', 'nr_prizes', 'population'])
+manager.create_api(Prize, results_per_page=1000, max_results_per_page=10000, methods=['GET'], exclude_columns=['search_text', 'url'])
+manager.create_api(Laureate, results_per_page=1000, max_results_per_page=10000, methods=['GET'], exclude_columns=['search_text', 'url'])
+manager.create_api(Country, results_per_page=1000, max_results_per_page=10000, methods=['GET'], exclude_columns=['search_text', 'url'])
 
 @app.route("/")
 def index():
--- search_db.py
+++ search_db.py
@@ -51,7 +51,6 @@ def oneInString(terms, searchString):
     return (inString, inStringList) 
 
 def findContexts(terms, searchString):
-    context = ""
     # find bound (inclusive, exclusive)
     min = len(searchString)
     max = -1
@@ -61,19 +60,12 @@ def findContexts(terms, searchString):
             min = i
         if i+len(query) > max:
             max = i+len(query)
-    words = re.compile("([.*] [.*] [.*]){0,3}")
-    matchBeg = words.search(searchString, 0, min)
-    matchEnd = words.search(searchString, max, len(searchString))
-    print(matchBeg.group(0)+searchString[min:max]+matchEnd.group(0))
-    for query in terms:
-        if query not in context:
-            match = re.search("(.{0,10})"+query+"(.{0,10})", searchString, re.IGNORECASE)
-            if match != None:
-                if "..." in context:
-                    context += match.group(0)+"..."
-                else:
-                    context += "..."+match.group(0)+"..."
-    return matchBeg.group(0)+searchString[min:max]+matchEnd.group(0)
+    
+    context = searchString[min:max]
+    words = re.compile("([^ ]*)( ?)([^ ]*)"+context+"([^ ]*)( ?)([^ ]*)", re.IGNORECASE)
+    match = words.search(searchString)    
+
+    return "..."+match.group(0)+"..."
 
 def searchTermOr(term):
     links = []
